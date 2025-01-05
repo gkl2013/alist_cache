fd3a77d6d1c6bc30a6b81513c256f560326bcde7
edenlguo <edenlguo@tencent.com>
2025-01-05 20:58:25 +0800

    feat: init

--- src/getConfig.js
+++ src/getConfig.js
@@ -1,1 +1,1 @@
-var o={protocol:"http",host:"101.35.113.109",port:5244,username:"test",password:"test"};const t=()=>(console.log("=======config",o,"object"),new Promise((t=>t(o))));export{t as getConfig};
+var o={protocol:"http",host:"101.35.113.109",port:5244,username:"emby",password:"emby"};const e=()=>(console.log("=======config",o,"object"),new Promise((e=>e(o))));export{e as getConfig};
